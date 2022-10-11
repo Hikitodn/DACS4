27dcb5230c7c6dbd07d198d223058094db4b4d8a
Hikitodn <Huy225dn@gmail.com>
2022-10-11 12:22:29 +0700

    setup folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
+const App:React.FC = () => {
   return (
     <div className="App">
-      
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/layouts/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
